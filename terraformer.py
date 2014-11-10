4bfafd05e0396263209ba35ec365152919f6fd99
Joshua Dong <jdong42@gmail.com>
2014-11-10 13:43:30 -0600

    Renamed variables in terraformer and enabled command blocks.

--- terraformer.py
+++ terraformer.py
@@ -8,44 +8,43 @@ def get_square_params():
     length = width
     height = 3#int(input("height: "))-1
     material = input("material (no minecraft: prefix): ")
-    return {'x': x, 'z': z, 'y_min': y_ground,
-            'x_2': x+length, 'y_max': y_ground+height, 'z_2': z+width,
+    return {'x_1': x, 'z_1': z, 'y_1': y_ground,
+            'x_2': x+length, 'z_2': z+width, 'y_2': y_ground+height,
             'material': material}
 
 def get_rectanglular_perimeter(params):
-    print("/fill {x} {y_min} {z} {x_2} {y_max} {z} minecraft:{material}".format(**params))
-    print("/fill {x} {y_min} {z} {x} {y_max} {z_2} minecraft:{material}".format(**params))
-    print("/fill {x_2} {y_min} {z} {x_2} {y_max} {z_2} minecraft:{material}".format(**params))
-    print("/fill {x} {y_min} {z_2} {x_2} {y_max} {z_2} minecraft:{material}".format(**params))
+    print("/fill {x_1} {y_1} {z_1} {x_2} {y_2} {z_2} minecraft:{material}".format(**params))
+    print("/fill {x_1} {y_1} {z_1} {x_1} {y_2} {z_2} minecraft:{material}".format(**params))
+    print("/fill {x_2} {y_1} {z_1} {x_2} {y_2} {z_2} minecraft:{material}".format(**params))
+    print("/fill {x_1} {y_1} {z_2} {x_2} {y_2} {z_2} minecraft:{material}".format(**params))
     print("")
 
 def get_rectanglular_prism(params):
-    print("/fill {x} {y_min} {z} {x_2} {y_max} {z_2} minecraft:{material}".format(**params))
+    print("/fill {x_1} {y_1} {z_1} {x_2} {y_2} {z_2} minecraft:{material}".format(**params))
     print("")
 
 def get_arena(params):
-    p = params.copy()
+    p_1 = params.copy()
     p_2 = params.copy()
 
-    get_rectanglular_prism(p)
+    get_rectanglular_prism(p_1)
 
     p_2['material'] = 'air'
-    p_2['x'] += 1
+
+    p_2['x_1'] += 1
     p_2['x_2'] -= 1
-    p_2['z'] += 1
+    p_2['z_1'] += 1
     p_2['z_2'] -= 1
     get_rectanglular_prism(p_2)
 
-    #get_rectanglular_perimeter(p)
-    p['y_max'] = p['y_min']-2
-    p['y_min'] -= 4
-    p['material'] = 'air'
-    get_rectanglular_prism(p)
+    p_1['material'] = 'air'
+    p_1['y_2'] = p_1['y_1']-2
+    p_1['y_1'] -= 4
+    get_rectanglular_prism(p_1)
 
-    p['y_max'] = p['y_min']-2
-    p['y_min'] -= 4
-    p['material'] = 'air'
-    get_rectanglular_prism(p)
+    p_1['y_2'] = p_1['y_1']-2
+    p_1['y_1'] -= 4
+    get_rectanglular_prism(p_1)
 
 if __name__ == "__main__":
     get_arena(get_square_params())
